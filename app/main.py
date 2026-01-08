8a3114b02da7d15b03a9bb138ec24c38a9795162
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2026-01-08 23:08:20 +0200

    feat: implement simple weater API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,34 @@
+import requests
+import os
+
+
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+WEATHERAPI_KEY = os.getenv("WEATHERAPI_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    if not WEATHERAPI_KEY:
+        print("Error: API_KEY is not set.")
+        return
+
+    print(f"Performing request to Weather API for city {CITY}...")
+
+    try:
+        r = requests.get(
+            BASE_URL,
+            params={"key": WEATHERAPI_KEY, "q": CITY}
+        )
+        r.raise_for_status()
+        weather_data = r.json()
+        print(
+            f"{weather_data['location']['tz_id']}",
+            f"{weather_data['location']['localtime']}",
+            f"Weather: {weather_data['current']['temp_c']} Celsius,",
+            f"{weather_data['current']['condition']['text']}"
+        )
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
